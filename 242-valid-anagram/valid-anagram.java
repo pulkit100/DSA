3b28a705b6644607b022ba20e6ced5027c2898e2
Pulkit Mishra <mishrapulkit715@gmail.com>
2026-07-09 15:24:21 +0530

    Time: 5 ms (61.61%) | Memory: 44.5 MB (67.06%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,5 +1,36 @@
 
-import java.util.HashMap;
+// import java.util.HashMap;
+// class Solution {
+//     public boolean isAnagram(String s, String t) {
+        
+
+//         if (s.length() != t.length()) {
+//             return false;
+//         }
+
+//         HashMap<Character, Integer> map = new HashMap<>();
+//         int n = s.length();
+
+//         for (int i=0; i<n; i++) {
+//             char ch = s.charAt(i);
+
+//             map.put(ch, map.getOrDefault(ch, 0) + 1);
+//         }
+
+//         for (int i=0; i<n; i++) {
+//             char ch = t.charAt(i);
+
+//             if (map.containsKey(ch) && map.get(ch) > 0) {
+//                 map.put(ch, map.get(ch) - 1);
+//             } else {
+//                 return false;
+//             }
+//         }
+        
+//         return true;
+//     }
+// }
+
 class Solution {
     public boolean isAnagram(String s, String t) {
         
@@ -7,25 +38,38 @@ class Solution {
             return false;
         }
 
-        HashMap<Character, Integer> map = new HashMap<>();
-        int n = s.length();
+        int[] freq = new int[26];
 
-        for (int i=0; i<n; i++) {
-            char ch = s.charAt(i);
+        for (int i=0; i<s.length(); i++) {
 
-            map.put(ch, map.getOrDefault(ch, 0) + 1);
+            freq[s.charAt(i) - 'a']++;
+            freq[t.charAt(i) - 'a']--;
         }
 
-        for (int i=0; i<n; i++) {
-            char ch = t.charAt(i);
-
-            if (map.containsKey(ch) && map.get(ch) > 0) {
-                map.put(ch, map.get(ch) - 1);
-            } else {
+        for (int i=0; i<26; i++) {
+            if (freq[i] != 0) {
                 return false;
             }
         }
-        
+
         return true;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
