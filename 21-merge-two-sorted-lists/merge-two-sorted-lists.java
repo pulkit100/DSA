3f0f8517721b70c79c3ab274fcf96915ed436ec6
Pulkit Mishra <mishrapulkit715@gmail.com>
2026-07-11 01:41:37 +0530

    Time: 0 ms (100.00%) | Memory: 44.5 MB (20.80%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.java
@@ -1,32 +1,77 @@
-import java.util.*;
+// import java.util.*;
+// class Solution {
+//     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        
+//         List<Integer> list = new ArrayList<>();
+
+//         ListNode temp1 = list1;
+//         ListNode temp2 = list2;
+        
+//         while ( temp1 != null ) {
+//             list.add(temp1.val);
+//             temp1 = temp1.next;
+//         }
+
+//         while ( temp2 != null ) {
+//             list.add(temp2.val);
+//             temp2 = temp2.next;
+//         }
+
+//         list.sort(null);
+
+//         ListNode dummy = new ListNode(-1);
+//         ListNode curr = dummy;
+
+//         for (int val : list) {
+//             curr.next = new ListNode(val);
+//             curr = curr.next;
+//         }
+
+//         return dummy.next;
+//     }
+// }
+
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        
-        List<Integer> list = new ArrayList<>();
 
-        ListNode temp1 = list1;
-        ListNode temp2 = list2;
-        
-        while ( temp1 != null ) {
-            list.add(temp1.val);
-            temp1 = temp1.next;
-        }
-
-        while ( temp2 != null ) {
-            list.add(temp2.val);
-            temp2 = temp2.next;
-        }
-
-        list.sort(null);
+        ListNode t1 = list1;
+        ListNode t2 = list2;
 
         ListNode dummy = new ListNode(-1);
         ListNode curr = dummy;
 
-        for (int val : list) {
-            curr.next = new ListNode(val);
-            curr = curr.next;
+        while ( t1 != null && t2 != null ) {
+            
+            if ( t1.val < t2.val ) {
+            
+                curr.next = t1;
+                curr = t1;
+                t1 = t1.next;
+            } else {
+
+                curr.next = t2;
+                curr = t2;
+                t2 = t2.next;
+
+            }
+        }
+
+        if ( t1 != null ) {
+            curr.next = t1;
+            
+        } 
+        if (t2 != null) {
+            curr.next = t2;
+            
         }
 
         return dummy.next;
     }
 }
+
+
+
+
+
+
+
