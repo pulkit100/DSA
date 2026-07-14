a5b2a03b6c789ce7b9515cbccac0c3d6db46c4a2
Pulkit Mishra <mishrapulkit715@gmail.com>
2026-07-15 01:26:31 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (82.29%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.java
+++ 191-number-of-1-bits/number-of-1-bits.java
@@ -1,13 +1,30 @@
+// class Solution {
+//     public int hammingWeight(int n) {
+        
+//         int count = 0;
+//         for (int i=31; i>=0; i--) {
+
+//             if ( (n>>i & 1) == 1 ) { // trick to get ith bit -- right sift any int by i and do & operation 
+//                 count++;
+//             }
+//         }
+
+//         return count;
+//     }
+// }
+
+
+// Better time complexity
 class Solution {
     public int hammingWeight(int n) {
-        
-        int count = 0;
-        for (int i=31; i>=0; i--) {
 
-            if ( (n>>i & 1) == 1 ) {
-                count++;
-            }
-        }
+        // approach 2 (50 & 49) -- it will unset the right most bit
+
+        int count = 0;
+        while ( n != 0 ) {
+            n = n & (n - 1);
+            count++;
+        }  
 
         return count;
     }
