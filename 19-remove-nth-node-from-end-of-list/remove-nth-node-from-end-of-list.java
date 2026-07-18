5a6d56b0b08c40580e9abcb972a07f1e62ef2642
Pulkit Mishra <mishrapulkit715@gmail.com>
2026-07-18 20:02:12 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (40.24%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
@@ -1,35 +1,63 @@
+// worst case time complexity will be - O(2 * length)
+// class Solution {
+//     public ListNode removeNthFromEnd(ListNode head, int n) {
+        
+//         ListNode temp = head;
+//         int count = 0;
 
+//         while ( temp != null) {
+//             count++;
+//             temp = temp.next;
+//         }
+//         // we are delteting a node from last node if n = 5 and count = 5 -- it will be 1st node
+//         if ( n == count) {
+//             return head.next;
+//         }
+
+//         int result = count - n;
+//         temp = head;
+
+//         while ( temp != null) {
+
+//             result--;
+//             if ( result == 0) {
+//                 break;
+//             }
+
+//             temp = temp.next;
+//         }
+
+//         ListNode delNode = temp.next;
+//         temp.next = temp.next.next;
+
+//         return head;
+//     }
+// }
+
+// slow and fast pointer krna hai to reduce time complexity
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        
-        ListNode temp = head;
-        int count = 0;
 
-        while ( temp != null) {
-            count++;
-            temp = temp.next;
+        ListNode fast = head;
+
+        for (int i=0; i<n; i++) {
+            fast = fast.next;
         }
-        // we are delteting a node from last node if n = 5 and count = 5 -- it will be 1st node
-        if ( n == count) {
+
+        ListNode slow = head;
+        if (fast == null) {
             return head.next;
         }
 
-        int result = count - n;
-        temp = head;
-
-        while ( temp != null) {
-
-            result--;
-            if ( result == 0) {
-                break;
-            }
-
-            temp = temp.next;
+        while (fast.next != null) {
+            slow = slow.next;
+            fast = fast.next;
         }
 
-        ListNode delNode = temp.next;
-        temp.next = temp.next.next;
+        ListNode delNode = slow.next;
+        slow.next = slow.next.next;
 
         return head;
     }
 }
+
