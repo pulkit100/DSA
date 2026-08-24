b6c3bee1d2c829b2bbdff6bd016b0df2641db142
Pulkit Mishra <mishrapulkit715@gmail.com>
2026-08-24 15:36:39 +0530

    Time: 30 ms (95.76%) | Memory: 75.9 MB (21.07%) - LeetSync

---
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        
+        int n = nums.length;
+        Arrays.sort(nums);
+
+        return nums[n-k];
+    }
+}
